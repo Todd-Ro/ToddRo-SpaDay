7b785c81483b451b426b1e4fad9cf1e778ec3654
toddr <todd.romkema@gmail.com>
2021-12-18 16:13:04 -0600

    Doing the best I can

---
+++ SpaDay/Controllers/UserController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SpaDay.Models;
+
+namespace SpaDay.Controllers
+{
+    public class UserController : Controller
+    {
+        static string mostRecentUsername;
+        public IActionResult Index()
+        {
+            ViewBag.username = mostRecentUsername;
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Add()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [Route("/User/Add")]
+        public IActionResult SubmitAddUserForm(
+            //User newUser, 
+            string verify
+            , string username, string email, string password
+            )
+        {
+            User newUser = new User(username, email, password);
+            if (verify.Equals(newUser.Password))
+            {
+                mostRecentUsername = newUser.Username;
+                ViewBag.username = newUser.Username;
+                return View("Index");
+            } else
+            {
+                return Redirect("/User/Add");
+            }
+        }
+    }
+}
---
+++ SpaDay/Models/User.cs
@@ -0,0 +1,26 @@
+﻿namespace SpaDay.Models
+{
+    public class User
+    {
+        internal string Username { get; }
+        string Email;
+        internal string Password { get; }
+
+        public User() 
+        { 
+            Username = "Default user";
+            Password = "password";
+        }
+        public User(string username, string email, string password)
+        {
+            Username = username;
+            Email = email;
+            Password = password;
+        }
+
+        public User(string username): this()
+        {
+            Username = username;
+        }
+    }
+}
